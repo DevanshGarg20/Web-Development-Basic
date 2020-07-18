f4d0c6f3f4a5795b2469ac40a542386725708bc3
Devansh Garg <50336366+DevanshGarg20@users.noreply.github.com>
2020-07-18 08:45:01 +0530

    initial commit

---
+++ Dog Years/app.js
@@ -0,0 +1,13 @@
+var myAge = 20
+// the following vaiable will store the early years
+var earlyYears = 2
+earlyYears *= 10.5
+//subtracting early years from my age
+var laterYears =myAge -2
+// concverting later years to dog years
+laterYears *= 4
+console.log(earlyYears)
+console.log(laterYears)
+myAgeInDogYears = earlyYears + laterYears
+myName = 'DEVANSH'.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`)
---
+++ Kelvin Weather/app.js
@@ -0,0 +1,9 @@
+//making constant variable kelvin
+const kelvin = 0
+//converting value to celcius
+var celcius = kelvin - 273
+//converting value to fahrenheit
+var fahrenheit = celcius*(9/5)+ 32
+//converting the value to integer
+fahrenheit = Math.floor(fahrenheit)
+console.log(`The temperature in ${fahrenheit} degrees Fahrenheit`)
